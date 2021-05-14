7e0de1ab5328aa3881d693dd1a73328b2e44d101
Max Nicolai <maxvnicolai@gmail.com>
2021-05-14 15:33:45 -0700

    removed test code from main app class

--- APP/ERS/src/main/java/me/max/ErsApplication.java
+++ APP/ERS/src/main/java/me/max/ErsApplication.java
@@ -9,18 +9,4 @@ public class ErsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ErsApplication.class, args);
 	}
-//	@RequestMapping("/")
-//		// Model session attr
-//		// Default serve index
-//	    public String index() {
-//	    	System.out.println(1);
-//	        return "index";
-//	    }
-//	    
-//	    @RequestMapping("hi")
-//	    public String home() {
-//	    	System.out.println(2);
-//	        return "index";
-//	    }
-
 }

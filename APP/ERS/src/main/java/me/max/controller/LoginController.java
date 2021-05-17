caf849e86760ca5081ff2ee357506a9419bcc985
Max Nicolai <maxvnicolai@gmail.com>
2021-05-16 20:59:44 -0700

    brought over dao methods, added spring jdbcTemplate to use in reconfiguring

--- APP/ERS/src/main/java/me/max/controller/LoginController.java
+++ APP/ERS/src/main/java/me/max/controller/LoginController.java
@@ -1,19 +1,33 @@
 package me.max.controller;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import me.max.models.User;
 
 //Will need session annotation, saving user once login is validated
-@Controller
+@RestController
 @RequestMapping("/")
 public class LoginController {
+	@Autowired
+	JdbcTemplate jdbcTemplate;
+	
 	// Model session attr
 	// Default serve index
     public String index() {
-    	System.out.println(1);
         return "index";
     }
 // Check user credentials
 // Send to appropriate page
+    @PostMapping("login")
+    public User loginUser(@RequestBody User attempt) {
+
+    	this.jdbcTemplate.queryForObject(null, null)
+    	
+    }
 // Can also handle logout here
 }
